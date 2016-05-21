0d02006ecc85005dfee5c01349ac192e6ff617c5
Yuta Ide <yIde4949@gmail.com>
2016-05-21 15:32:55 +0900

    ダイナモ感覚

--- backend/main.js
+++ backend/main.js
@@ -4,6 +4,22 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB({region: 'us-west-2'});
+
+var params = {
+    ExclusiveStartTableName: 'STRING_VALUE',
+    Limit: 10
+};
+
+dynamodb.listTables(params, function(err, data) {
+    if (err) {
+        console.log(err, err.stack); // an error occurred
+    } else {
+        console.log(data);           // successful response
+    }
+});
+
 //apiサーバーへのルーティング定義
 var api = require( './routes/api' );
 app.use('/api', api);
