831d7419bfa73bee10a0711d9d54768f51f7f4d2
IzumiSy <beetle-noise@gmx.com>
2016-05-22 02:05:59 +0900

    Improve

--- backend/main.js
+++ backend/main.js
@@ -1,15 +1,19 @@
 // サーバーサイド
 // 必要なパッケージの読み込み
 var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
 var mongoose = require("mongoose");
+var api = require('./routes/api');
+var router = express.Router();
+var app = express();
 
 mongoose.connect('mongodb://localhost/test');
 
-//apiサーバーへのルーティング定義
-var api = require('./routes/api');
-app.use('/api', api);
+app.set('port', process.env.PORT || 3000)
+app.use(app.router)
+
+// apiサーバーへのルーティング定義
+router.get(api.getEvents.path, api.getEvents.handler);
 
 app.listen(port);
+
 console.log('listen on port ' + port);
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,20 +1,17 @@
-var express = require('express');
-var router = express.Router();
 var mongoose = require('mongoose');
-var Event = require("../models/event"); //モデルの宣言
-var app = express();
+var Event = require("../models/event");
 
-// GET index
-/*
-  サンプルデータ投入
-  db.event.save({name:"araki",event:"eat"})
-*/
-router.get('/events',function(req, res){
-  console.log("一覧表示命令来た");
-  Event.find({}, function(err, events) {
-    res.json(events);
-    console.log(events)
-  });
-});
-
-module.exports = router;
+module.exports = {
+  getEvents: {
+    path: '/events',
+    handler: function(req, res) {
+      console.log("一覧表示命令来た");
+      Event.find({
+        date: req.query.date || undefined
+      }, function(err, events) {
+        res.json(events);
+        console.log(events)
+      });
+    }
+  }
+};
