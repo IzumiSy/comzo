3048e5987c6dc466dd0af42dc32369ce2c2317dd
IzumiSy <beetle-noise@gmx.com>
2016-05-22 03:18:32 +0900

    Use corser

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,7 @@
 // サーバーサイド
 // 必要なパッケージの読み込み
 var express = require('express');
+var corser = require('corser');
 var app = express();
 var port = process.env.PORT || 3000;
 var mongoose = require("mongoose");
@@ -11,5 +12,6 @@ mongoose.connect('mongodb://localhost/test:27017');
 var api = require('./routes/api');
 app.use('/api', api);
 
+app.use(corser.create());
 app.listen(port);
 console.log('listen on port ' + port);
