802de249c4997c170bc51f89bbaa3cbb20d98e48
IzumiSy <beetle-noise@gmx.com>
2016-05-21 19:33:31 +0900

    Implement slider component

--- client/app/root.js
+++ client/app/root.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
 import template from './root.html!text'
 import mapComponent from './map.js'
+import sliderComponent from './slider.js'
 
 export default {
   template: template,
 
   components: {
-    'map-view': mapComponent
+    'map-view': mapComponent,
+    'hour-slider': sliderComponent
+  },
+
+  events: {
+    'slider:hour:changed': function(data) {
+      // Do something
+    },
+
+    'datepicker:date:changed': function(data) {
+      // Do something
+    }
   },
 
   created() {
