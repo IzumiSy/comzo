58769691d831c49807f747768c261872501d3e56
Yuta Ide <yIde4949@gmail.com>
2016-05-22 13:06:26 +0900

    c3のグラフもできた. 動的制約は今からする.

--- client/app/chart/chart.js
+++ client/app/chart/chart.js
@@ -10,16 +10,46 @@ export default {
   props: ["date"],
 
   ready() {
-    API.FetchWeekSummary(this.data.date).then((response) => {
-      this.renderCharts()
+    API.FetchWeekSummary(this.date).then((response) => {
+      this.renderCharts(response)
     }).catch((response) => {
       // TODO error handling
     })
   },
 
   methods: {
-    renderCharts() {
-      // Do something here
+    renderCharts(res) {
+      console.log(res["data"]);
+      console.log(res["data"].length);
+      console.log(res["data"][0]);
+      console.log(res["data"][0]["sum"]);
+      var rendering_data = ["混雑度"]
+      for(var i=0; i<res["data"].length; i++){
+        rendering_data.push(res["data"][i]["sum"])
+      }
+      console.log(rendering_data)
+      console.log("上のレンダリン具データ")
+       // Do something here
+       var chart = c3.generate({
+       bindto: '#chart',
+       data: {
+         columns: [
+           ["混雑度", 79646, 45133, 67683, 50858, 112372, 159179, 162147],
+           ["平均", 120000, 120000, 120000, 120000, 120000, 120000, 120000]
+         ],
+         types: {
+            混雑度: 'spline',
+            平均: 'spline'
+        }
+       },
+       axis: {
+         y: {
+           label: { // ADD
+             text: 'Y Label'
+           }
+         }
+       }
+   });
     }
   }
 }
